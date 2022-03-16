780ef3e9ec5d7cbaf0f74eab63facb1de7b0641d
AlesyaD <alesya.dubik.99@mail.ru>
2022-03-16 13:03:46 +0300

    fetch

--- src/components/Fetch.jsx
+++ src/components/Fetch.jsx
@@ -3,32 +3,30 @@ import React, {useState, useEffect} from "react"
 
 export default function Fetch(){
     const [isLoading, setIsLoading] = useState(true)
-    const [musicGroup, setMusicGroup] = useState([])
+    const [news, setNews] = useState([])
     const [errorMessage, setErrorMessage] = useState("")
     
     useEffect(() => {
         setIsLoading(true)
-        fetch("http://musicbrainz.org/ws/2/artist/5b11f4ce-a62d-471e-81fc-a69a8278c7da?fmt=json")
+        fetch("https://inshortsapi.vercel.app/news?category=world")
         .then((response) => {
             if(response.status !== 200){
                 throw Error(response.statusText)
             }
             return response.json()})
         .then((data) => {
-            setMusicGroup(data)
+            setNews(data)
         })
         .catch((error) => setErrorMessage(error.message))
         .finally(() => setIsLoading(false));
     }, [])
-
     return (
         <div className="music-group">
             <div>{isLoading && "Loading..."}</div>
             <div>{errorMessage}</div>
-            <div className="group-name">{musicGroup.name}</div>
-            <div className="group-age">{musicGroup["life-span"].begin.slice(0, 4)}-{musicGroup["life-span"].end.slice(0, 4)}</div>
-            <div className="group-style">{musicGroup.disambiguation}</div>
-            <div className="group-place">{musicGroup.area.name}</div>
+            <div className="group-name">{news.data[1].title}</div>
+            <div className="group-age">{news.data[1].date}</div>
+            <div className="group-style">{news.data[1].content}</div>
         </div>
     )
 }
